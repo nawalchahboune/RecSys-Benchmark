d3b309956872be1faf55feb2508935d50c2bb6a4
mahima-jpatel <mahimapateladm@gmail.com>
2025-07-26 15:13:01 -0400

    Update get_queries.sh

--- ClueWeb-Reco/get_queries.sh
+++ ClueWeb-Reco/get_queries.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=SASRec_ml1m
+#SBATCH --job-name=get_queries_together_deepseek
 #SBATCH --output=outputs/%x-%j.out
 #SBATCH --error=outputs/%x-%j.err 
 #SBATCH --partition=general 
@@ -9,13 +9,12 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16G
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --time=48:00:00
 #SBATCH --mail-type=END
-#SBATCH --mail-user="jingyuah@cs.cmu.edu"
+#SBATCH --mail-user="mjagadee@andrew.cmu.edu"
 
 model="together"
 nproc=2
 
-python3 get_queries.py \
-    --model "$model"
+python3 cw_get_queries.py --model "$model"
